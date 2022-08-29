d19c1e0966e065241c96c2b58c2f89993856dbb7
Denis Ivanov <mr.ivanov667@yandex.ru>
2022-08-29 11:28:45 +0500

    Init

---
+++ src/components/Post.vue
@@ -0,0 +1,18 @@
+<template>
+  <article class="post">
+    <h2 class="post__title">{{item.title}}</h2>
+    <p class="post__body">{{item.body}}</p>
+    <div class="post__user">{{item.username}}</div>
+  </article>
+</template>
+
+<script setup lang="ts">
+import type {Ipost} from "@/App.vue";
+const props = defineProps<{
+  item: Ipost
+}>()
+</script>
+
+<style scoped lang="scss">
+@import "../assets/post";
+</style>
